3a5cbb1ccb1acc5a01617532cb7c5f9bd8e678ec
Nikhil Prashant <nikhilprashant1@gmail.com>
2023-06-23 11:25:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 //your code here
+let bandNames = ['The Beatles', 'Led Zeppelin', 'Pink Floyd', 'The Rolling Stones', 'Metallica', 'Nirvana'];
+
+function removeArticles(name) {
+  name = name.replace(/^(a|an|the)\s+/i, '');
+
+  name = name.replace(/\s+(a|an|the)$/i, '');
+
+  return name;
+}
+
+bandNames.sort(function(a, b) {
+  a = removeArticles(a.toLowerCase());
+  b = removeArticles(b.toLowerCase());
+
+  return a.localeCompare(b);
+});
+
+let bandList = document.getElementById('band');
+
+for (let i = 0; i < bandNames.length; i++) {
+  let listItem = document.createElement('li');
+  listItem.textContent = bandNames[i];
+  bandList.appendChild(listItem);
+}
 
